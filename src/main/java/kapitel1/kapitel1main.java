dcdb4ede90e4a5144335a4fb7977fb9c765ecc82
Rahim Akkoyun <rahim.akkoyun@outlook.com>
2024-07-26 00:46:11 +0200

    Rahim Akkoyun, Übung 1.1.1

---
+++ src/main/java/kapitel1/kapitel1main.java
@@ -0,0 +1,11 @@
+package kapitel1;
+
+public class kapitel1main
+
+{
+    public static void main(String[] args) {
+        uebung111 utility = new uebung111();
+
+        utility.printMessage();
+    }
+}
---
+++ src/main/java/kapitel1/uebung111.java
@@ -0,0 +1,16 @@
+package kapitel1;
+
+public class uebung111
+
+{
+    /**
+     * Diese Methode gibt den String "Aye Captain!" aus.
+     * @author Rahim Akkoyun
+     * @param none
+     * @return none
+     */
+    public void printMessage()
+    {
+        System.out.println("Aye Captain!");
+    }
+}
